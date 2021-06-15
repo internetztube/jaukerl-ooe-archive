fee2567554bb2e5aea242d6a1fd63feacd4248f1
Frederic Koeberl <frederic.koeberl@dynatrace.com>
2021-06-15 20:54:37 +0200

    Initial Commit

--- index.sh
+++ index.sh
@@ -1,11 +1,38 @@
 #!/usr/bin/env sh
 
-FILENAME=at-ooe-`date +"%Y-%m-%dT%H:%M:%S%z"`.json
+# 2020-06-15T20:50:26+0200.json
+FILENAME=`date +"%Y-%m-%dT%H:%M:%S%z"`.json
+
+# 2020/06/15
 FOLDER=repo/data/`date +%Y/%m/%d/`
+
+# Cleanup
 rm -rf repo
+
+# Fetch Repo from remote.
 git clone git@github.com:internetztube/jaukerl-ooe-archive.git repo
+
+# Fetch new data entry.
 curl https://jaukerl-ooe-api.m8.at/ > $FILENAME
+
+# Ensure the result folder is present.
 mkdir -p $FOLDER
+
+# Move file into final folder.
 mv $FILENAME $FOLDER$FILENAME
+
+# Only commit files in repo/data.
+cd repo/data
+
+# Add files to git.
 git add .
+
+# Commit!
 git commit -m "Add $FILENAME"
+
+# And finally push!
+git push -u origin master
+
+# Last step. Cleanup, again.
+cd ../.. && rm -rf repo
+
