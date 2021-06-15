2973a404215d188959b6f017db4f57a48d42c999
Frederic Koeberl <frederic.koeberl@dynatrace.com>
2021-06-15 20:47:17 +0200

    Add at-ooe-2021-06-15T20:47:12+0200.json

--- index.sh
+++ index.sh
@@ -1,4 +1,11 @@
-#!/usr/bin/env zx
+#!/usr/bin/env sh
 
-$`rm -rf repo`
-$`git clone `
+FILENAME=at-ooe-`date +"%Y-%m-%dT%H:%M:%S%z"`.json
+FOLDER=repo/data/`date +%Y/%m/%d/`
+rm -rf repo
+git clone git@github.com:internetztube/jaukerl-ooe-archive.git repo
+curl https://jaukerl-ooe-api.m8.at/ > $FILENAME
+mkdir -p $FOLDER
+mv $FILENAME $FOLDER$FILENAME
+git add .
+git commit -m "Add $FILENAME"
